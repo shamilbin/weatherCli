328e28af9dd6d8e729342bc77f160a5716aebb58
shamilbin <shamiltk029@gmail.com>
2025-04-04 23:16:05 +0530

    weather report

---
+++ server/app.js
@@ -0,0 +1,69 @@
+
+
+import readline from "readline-sync";
+import axios from "axios";
+import dotenv from "dotenv";
+import chalk from "chalk";
+import got from "got";
+
+dotenv.config();
+
+async function call() {
+  try {
+    console.clear();
+    let cityName = readline.question(chalk.bold.green("Enter your city :"));
+    let apiKey = process.env.KEY;
+
+    if (!cityName) {
+      console.clear();
+      console.log(chalk.bold.red("🔎 Enter a valid city name :"));
+    }
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+    let response = await axios.get(url);
+
+    console.clear();
+    console.log(chalk.bold.cyanBright(` 🌥️  Weather Report of ${cityName}`));
+    // console.log(response.data);
+
+    // console.log(`${chalk.bold.yellow("📍 Location           :")}${location}`);
+    // console.log(`${chalk.bold.yellow("👥 Followers          :")}${followers}`);
+    // console.log(`${chalk.bold.yellow("🔁 Following          :")}${following}`);
+
+    console.log(
+      `${chalk.bold.yellow(`\n\n The temperature in ${cityName} is  :`)}${
+        response.data.main.temp
+      }`
+    );
+    console.log(
+      `${chalk.bold.yellow(` The Pressure in ${cityName} is     :`)}${
+        response.data.main.pressure
+      }`
+    );
+    console.log(
+      `${chalk.bold.yellow(` The Humidity in ${cityName} is     :`)}${
+        response.data.main.humidity
+      }`
+    );
+    console.log(
+      `${chalk.bold.yellow(` The Discription of ${cityName} is  :`)}${
+        response.data.weather.description
+      }`
+    );
+    console.log(
+      `${chalk.bold.yellow(` The Latitude of ${cityName} is     :`)}${
+        response.data.coord.lat
+      }`
+    );
+    console.log(
+      `${chalk.bold.yellow(` The Longtitude of ${cityName} is   :`)}${
+        response.data.coord.lon
+      }\n\n`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+call();
